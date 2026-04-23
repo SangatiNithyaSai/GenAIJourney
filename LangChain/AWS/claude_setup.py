b99b50b38e7b11403432bcf6812ab2891710ee17
SangatiNithyaSai <nithyasaitejareddy99@gmail.com>
2026-04-23 22:57:38 +0530

    AWS bedrock

---
+++ LangChain/AWS/claude_setup.py
@@ -0,0 +1,25 @@
+import boto3
+import json
+prompt_data=""" 
+Act as a mathematican and explain Generative AI
+"""
+
+bedrock=boto3.client(service_name="bedrock-runtime",region_name='us-east-1')
+
+payload={
+    "prompt":prompt_data,
+    "maxTokens":512,
+    "temperature":0.8,
+    "topP":0.8
+}
+body=json.dumps(payload)
+model_id="amazon.nova-micro-v1:0"
+response=bedrock.invoke_model(
+    body=body,
+    modelId=model_id,
+    accept="application/json",
+    contentType="application/json"
+)
+response_body=json.loads(response.get("body").read())
+response_text=response_body.get("completions")[0].get("data").get("text")
+print("response:",response_text)
